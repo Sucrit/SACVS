f3eeb5cc8bb7b7f9ddda2b7d7db243cb17b9cc2e
Sucrit <smitholiver106@gmail.com>
2026-01-26 15:42:17 +0300

    user service health checkl

--- backend/services/user-service/src/app.ts
+++ backend/services/user-service/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api', userRoutes);
+app.use('/', userRoutes);
 
 app.use(errorHandler);
 
--- backend/services/user-service/src/routes/user.routes.ts
+++ backend/services/user-service/src/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { UserController } from '../controller/user.controller';
+import { healthCheck } from '../controller/health.controller';
 
 const router = Router();
 
+router.get('/users/health', healthCheck);
 router.get('/users', UserController.getUsers);
 router.post('/users', UserController.createUser);
 
